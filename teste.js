9d8bc24cc4c098e633c728e70b55a4ece338a04c
CaioCasemiro <casemirocaio2@gmail.com>
2024-08-14 13:54:04 -0300

    Projeto Gerenciador de Tarefas

--- teste.js
+++ teste.js
@@ -1,17 +1,19 @@
-/*const clearTerminal = () => {
-    process.stdout.write('\x1Bc'); 
-};
+let listadefilmes = [{titulo: 'case', data: 27}, {titulo: 'kaio mourato', data: 27}, {titulo: 'widi', data: 27}]
+let filme = prompt('nome: ')
+function pesquisarTarefa (filme){
+    let presente = false
+    for(let i = 0; i < listadefilmes.length; i++){
+        if (listadefilmes[i].titulo.includes(filme)){
+            presente = true
+            break
+        }
+    }
+    if (presente === true){
+        return console.log(`Há um filme presente na lista com o nome ${filme}!`)
+    }
+    else{
+        return console.log(`Verifique a ortografia pois não há nenhum filme com o nome ${filme} na lista!`)
+    }
+}
 
-
-clearTerminal();*/
-
-let lista = ['Caio', 'Caze', 'preto']
-let conclusao = []
-
-
-let nome = lista.splice(1,1)
-conclusao.push(nome)
-
-console.log(nome)
-console.log(lista)
-console.log(conclusao)
+pesquisarTarefa()
